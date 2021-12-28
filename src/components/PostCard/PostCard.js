9ba6e99a814d7c36d8e6e028c9320236ef5cb40e
tsvetislav <tsvetislavt99@gmail.com>
2021-12-28 14:21:23 +0200

    fix date on PostCard

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -13,7 +13,9 @@ function PostCard({ post }) {
           <h5 className='post-title'>
             <Link to={`/blog/${post._id}`}>{post.title}</Link>
           </h5>
-          <div className='post-date'>Posted on {post.dateOfCreation}</div>
+          <div className='post-date'>
+            Posted on {post.dateOfCreation.substring(0, 10)}
+          </div>
         </div>
       </div>
     </div>
